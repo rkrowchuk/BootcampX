e9fd224e42a5a91267a28e10cd0d2b34e2582083
rkrowchuk <rhiannonkrowchuk@gmail.com>
2022-03-09 18:01:39 -0800

    create students.js and teachers.js

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,25 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+pool.query(`
+SELECT teachers.name as teacher, cohorts.name as cohort, count(assistance_requests.*) as total_assistances
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name LIKE '%${process.argv[2]}%'
+GROUP BY teacher, cohort
+ORDER BY teacher;
+`)
+.then(res => {
+  res.rows.forEach(user => {
+    console.log(`${user.cohort}: ${user.teacher}`);
+  })
+})
+.catch(err => console.error('query error', err.stack));
